6027d2c00e185931a3b62b2cace888677f210df1
Zdroba <72391375+DariusZdroba@users.noreply.github.com>
2024-04-13 22:22:56 +0300

    Add files via upload

---
+++ SnykProblem8/SnykProblem8/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SnykProblem8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? s = Console.ReadLine();
+
+            if(s != null)
+            {
+                Encrypt(s);
+            }
+        }
+
+        static void Encrypt(string s)
+        {
+            s = s.Replace(" ", "");
+
+            int floor = (int)Math.Floor(Math.Sqrt(s.Length));
+            int ceil = (int)Math.Ceiling(Math.Sqrt(s.Length));
+            
+            int iterator = 0;
+
+            for(int i=0; i < floor; i++)
+            {
+                for(int j=0; j < ceil && iterator < s.Length; j++)
+                {
+                    Console.Write(s[iterator]);
+                    iterator++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
